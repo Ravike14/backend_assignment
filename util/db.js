67fee8aa1097e6a8c9e3ae0534caa0538d4acbf9
Ravishka Fernando <ravi133@live.com>
2022-03-26 21:50:00 +0530

    Update db.js

--- util/db.js
+++ util/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   user: "root",
   port: 3306,
   database: "trans",
-  password: "Rav@2019!.",
+  password: "",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
